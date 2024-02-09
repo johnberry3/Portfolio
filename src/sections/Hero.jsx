26fde8cf6d8eadabcc36e1418cf3df18d661b3a8
johnberry3 <jmalachy727@gmail.com>
2024-02-09 12:15:11 +0100

    Added changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MainProfile } from '../assets/image'
 import { IoDownloadOutline } from "react-icons/io5";
+import { IoMdContact } from "react-icons/io";
 
 
 const Hero = () => {
@@ -10,11 +11,11 @@ const Hero = () => {
            <div className='top-[30%]  max-xl:top-[20%] max-xl:text-center sm:10% relative'>
            <h2 className='text-5xl max-sm:text-3xl font-bold  '>Hello! I'm Johnberry</h2>
             <h2 className='text-3xl max-sm:text-2xl font-bold mt-4'>A Frontend Developer</h2>
-            <p className='mt-4 max-sm:mt-2 text-justify max-xl:text-center italic text-gray-600'>As a frontend developer, I specialize in crafting delightful user experiences and bringing designs to life through code. With a keen eye for design aesthetics and a knack for problem-solving, I thrive on turning ideas into interactive and responsive web applications.</p>
+            <p className='mt-4 max-sm:mt-2 text-justify max-xl:text-center font-mono italic text-gray-600'>As a frontend developer, I specialize in crafting delightful user experiences and bringing designs to life through code. With a keen eye for design aesthetics and a knack for problem-solving, I thrive on turning ideas into interactive and responsive web applications.</p>
            <div className='flex items-center max-xl:justify-center  gap-5 mt-5'>
            <button className='bg-black max-sm:text-[10px] uppercase text-sm  px-3 flex items-center gap-2 font-bold py-2 text-white '>Download Resume
             <IoDownloadOutline /></button>
-            <button className='px-3 py-2 border-black border-2 max-sm:text-[10px] uppercase font-semibold'>Scroll </button>
+            <button className='px-3 py-2 flex gap-5 items-center border-black border-2 max-sm:text-[10px] uppercase font-semibold'>Contact Me <IoMdContact /> </button>
            </div>
            </div>
            <div>
