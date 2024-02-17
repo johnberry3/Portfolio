c76d89f149f9302f890e13e34150a690afe81b70
johnberry3 <jmalachy727@gmail.com>
2024-02-17 12:01:30 +0100

    Added changes

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FaFacebook } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { RiMenu3Fill } from "react-icons/ri";
+import { AiOutlineClose } from "react-icons/ai";
 
 
 const Navbar = () => {
+   const [click, setClick]=useState(false)
+
+   const handleClick = ()=> setClick(!click);
+
+   const content = <>
+       <ul className='bg-black absolute right-0 justify-center  max-sm:top-[5%] md:top[5%] overflow-hidden w-[300px] min-h-screen z-10 text-center flex flex-col gap-11  items-center'>
+          <a href=""><li className='text-2xl  font-mono uppercase'>Home</li></a>
+          <a href=""><li className='text-2xl  font-mono uppercase'>About</li></a>
+          <a href=""><li className='text-2xl  font-mono uppercase'>Project</li></a>
+          <a href=""><li className='text-2xl  font-mono uppercase'>Contact</li></a>
+          <div className='flex gap-11 items-center mt-9'>
+      <a href=""><FaGithub color='white'  /></a>
+         <a href=""><FaFacebook  color='white'/></a>
+         <a href=""><FaXTwitter color='white' /></a>
+         <a href=""><FaLinkedinIn color='white' /></a>
+      </div>
+       </ul>
+      
+   </>;
+
   return (
     <div className='flex justify-between items-center text-white'>
       <div>
@@ -27,11 +48,14 @@ const Navbar = () => {
          <a href=""><FaXTwitter color='white' /></a>
          <a href=""><FaLinkedinIn color='white' /></a>
       </div>
-      <div className='menu-bar hidden max-xl:block'>
-          <RiMenu3Fill color='white'  fontSize={30}/>
+      <div className='hidden max-lg:block'>
+          {click && content}
+      </div>
+      <div className='menu-bar hidden max-lg:block'>
+          {click ? <AiOutlineClose color='white' fontSize={30} onClick={handleClick} /> : <RiMenu3Fill color='white'  fontSize={30} onClick={handleClick}/>}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar
--- src/sections/Project.jsx
+++ src/sections/Project.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import { Github } from '../assets/image'
 
 const Project = () => {
   return (
-    <div>
-        <div>
-            <h2 className='text-3xl font-bold uppercase text-white'>My Projects</h2>
+    <div className='text-white flex flex-col gap-10'>
+      <h2 className='text-3xl font-bold uppercase  text-white'>My Projects</h2>
+        <div className='w-[1100px] max-xl:w-full flex flex-col gap-8 justify-center m-auto max-xl-m-auto'>
+            <div className='flex max-xl:flex-col justify-between items-center'>
+               <div>
+                  <h2 className='text-4xl font-bold '>Lorem ipsum dolor, sit amet</h2>
+                  <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+                 <img src={Github} className='w-[600px]  h-[300px]' alt="Github" />
+            </div>
+
+            <div className='flex max-xl:flex-col flex-row-reverse justify-between items-center'>
+               <div>
+                  <h2 className='text-4xl font-bold '>Lorem ipsum dolor, sit amet</h2>
+                  <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+               <img src={Github} className='w-[600px] h-[300px]' alt="Github" />
+            </div>
+
+            <div className='flex max-xl:flex-col justify-between items-center'>
+               <div>
+                  <h2 className='text-4xl font-bold '>Lorem ipsum dolor, sit amet</h2>
+                  <p className='text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+               </div>
+               <img src={Github} className='w-[600px] h-[300px]' alt="Github" />
+            </div>
         </div>
     </div>
   )
